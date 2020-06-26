ed769fa2cf0833bbaadf20976066322ac922be76
techcoderx <engtianxi@outlook.com>
2020-06-26 12:30:40 +0800

    add avalon decryption function

---
+++ decrypt.py
@@ -0,0 +1,44 @@
+from Crypto.Cipher import AES
+import eth_utils
+import hashlib
+import hmac
+import secp256k1
+import base58
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.backends import default_backend
+
+def ecies_decrypt(priv_key, message_parts):
+    sender_public_key_obj = ec.EllipticCurvePublicKey.from_encoded_point(ec.SECP256K1(), message_parts["ephemPublicKey"])
+    private_key_obj = ec.derive_private_key(eth_utils.to_int(priv_key),ec.SECP256K1(),default_backend())
+    aes_shared_key = private_key_obj.exchange(ec.ECDH(), sender_public_key_obj)
+    # Now let's do AES-CBC with this, including the hmac matching (modeled after eccrypto code).
+    aes_keyhash = hashlib.sha512(aes_shared_key).digest()
+    hmac_key = aes_keyhash[32:]
+    test_hmac = hmac.new(hmac_key, message_parts["iv"] + message_parts["ephemPublicKey"] + message_parts["ciphertext"], hashlib.sha256).digest()
+    if test_hmac != message_parts["mac"]:
+        raise Exception("Mac does not match")
+    aes_key = aes_keyhash[:32]
+    # Actual decrypt is modeled after ecies.utils.aes_decrypt() - but with CBC mode to match eccrypto.
+    aes_cipher = AES.new(aes_key, AES.MODE_CBC, iv=message_parts["iv"])
+    try:
+        decrypted_bytes = aes_cipher.decrypt(message_parts["ciphertext"])
+        # Padding characters (unprintable) may be at the end to fit AES block size, so strip them.
+        unprintable_chars = bytes(''.join(map(chr, range(0,32))).join(map(chr, range(127,160))), 'utf-8')
+        decrypted_string = decrypted_bytes.rstrip(unprintable_chars).decode("utf-8")
+        return decrypted_string
+    except:
+        raise Exception("Could not decode ciphertext")
+
+# Converts encrypted message from jAvalon to Python compatible encrypted object
+def js_to_py_encrypted(js_encrypted_msg):
+    encrypted_b58_lst = js_encrypted_msg.split('_')
+    ephemPubKey_obj = secp256k1.PublicKey().deserialize(base58.b58decode(encrypted_b58_lst[1]))
+    
+    py_encrypted_obj = {
+        'iv': base58.b58decode(encrypted_b58_lst[0]),
+        'ephemPublicKey': secp256k1.PublicKey(pubkey=ephemPubKey_obj).serialize(compressed=False),
+        'ciphertext': base58.b58decode(encrypted_b58_lst[2]),
+        'mac': base58.b58decode(encrypted_b58_lst[3])
+    }
+
+    return py_encrypted_obj
