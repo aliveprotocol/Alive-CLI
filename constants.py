69f7d58b6047ede7a50856b65c6a403338db3fd5
techcoderx <engtianxi@outlook.com>
2021-01-16 12:42:56 +0800

    updated constants

--- constants.py
+++ constants.py
@@ -1,12 +1,18 @@
 # Skynet upload portals (first index is default)
 # TODO: Updated portal list
 skynet_upload_portals = [
-    'https://siasky.net'
+    'https://siasky.net',
+    'https://us-west.siasky.net',
+    'https://skyportal.xyz',
+    'https://www.siacdn.com',
+    'https://skynethub.io',
+    'https://sialoop.net'
 ]
 
 # IPFS upload endpoints with authentication required
 authenticated_ipfs_upload_endpoints = [
-    "http://localhost:3000"
+    'https://uploader.oneloved.tube',
+    'http://localhost:3000'
 ]
 
 valid_protocols = ['IPFS','Skynet']
