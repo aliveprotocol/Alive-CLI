86ea69816808a1bd1655b30d6116fac4b793a250
techcoderx <engtianxi@outlook.com>
2021-02-01 19:30:44 +0800

    configure + end stream cli hive

--- stream_configure.py
+++ stream_configure.py
@@ -1,43 +1,60 @@
-import json
-import time
 import sys
-import requests
-import base58
-import hashlib
-import secp256k1
 
-if len(sys.argv) != 6:
-    print('Usage: ' + sys.argv[0] + ' <avalon_api_node> <link> <alivedb_pubkey> <username> <private_key>')
+if len(sys.argv) != 7:
+    print('Usage: ' + sys.argv[0] + ' <network> <avalon_api_node> <link> <alivedb_pubkey> <username> <private_key>')
     sys.exit(0)
 
-api = sys.argv[1]
-link = sys.argv[2]
-pub = sys.argv[3]
-sender = sys.argv[4]
-key = sys.argv[5]
+api = sys.argv[2]
+link = sys.argv[3]
+pub = sys.argv[4]
+sender = sys.argv[5]
+key = sys.argv[6]
 
-tx = {
-    'type': 21,
-    'data': {
+if sys.argv[1] == 'dtc':
+    # Avalon
+    import json
+    import time
+    import requests
+    import base58
+    import hashlib
+    import secp256k1
+
+    tx = {
+        'type': 21,
+        'data': {
+            'link': link,
+            'pub': pub
+        },
+        'sender': sender,
+        'ts': round(time.time() * 1000)
+    }
+
+    txString = json.dumps(tx,separators=(',',':'))
+    tx['hash'] = hashlib.sha256(txString.encode('UTF-8')).hexdigest()
+
+    pk = secp256k1.PrivateKey(base58.b58decode(key))
+    hexhash = bytes.fromhex(tx['hash'])
+    sign = pk.ecdsa_sign(hexhash,raw=True,digest=hashlib.sha256)
+    signature = base58.b58encode(pk.ecdsa_serialize_compact(sign)).decode('UTF-8')
+    tx['signature'] = signature
+
+    headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+    }
+    broadcast = requests.post(api + '/transact',data=json.dumps(tx,separators=(',',':')),headers=headers)
+    print(broadcast.text)
+elif sys.argv[1] == 'hive':
+    # Hive
+    from beem import Hive
+    hive_client = Hive(node=api,keys=[key])
+
+    json_data = {
+        'op': 2,
         'link': link,
         'pub': pub
-    },
-    'sender': sender,
-    'ts': round(time.time() * 1000)
-}
+    }
 
-txString = json.dumps(tx,separators=(',',':'))
-tx['hash'] = hashlib.sha256(txString.encode('UTF-8')).hexdigest()
-
-pk = secp256k1.PrivateKey(base58.b58decode(key))
-hexhash = bytes.fromhex(tx['hash'])
-sign = pk.ecdsa_sign(hexhash,raw=True,digest=hashlib.sha256)
-signature = base58.b58encode(pk.ecdsa_serialize_compact(sign)).decode('UTF-8')
-tx['signature'] = signature
-
-headers = {
-    'Accept': 'application/json, text/plain, */*',
-    'Content-Type': 'application/json'
-}
-broadcast = requests.post(api + '/transact',data=json.dumps(tx,separators=(',',':')),headers=headers)
-print(broadcast.text)
+    hive_client.custom_json('alive-test',json_data,required_posting_auths=[sender])
+else:
+    raise ValueError('Invalid network')
