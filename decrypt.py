7fd46e7f73102f710060784b26859e6ed952eda7
techcoderx <engtianxi@outlook.com>
2022-05-29 10:32:29 +0800

    cleanup decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,15 +1,15 @@
 from Crypto.Cipher import AES
-import eth_utils
 import hashlib
 import hmac
 import secp256k1
 import base58
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.backends import default_backend
+from typing import Union
 
 def ecies_decrypt(priv_key, message_parts):
     sender_public_key_obj = ec.EllipticCurvePublicKey.from_encoded_point(ec.SECP256K1(), message_parts["ephemPublicKey"])
-    private_key_obj = ec.derive_private_key(eth_utils.to_int(priv_key),ec.SECP256K1(),default_backend())
+    private_key_obj = ec.derive_private_key(to_int(priv_key),ec.SECP256K1(),default_backend())
     aes_shared_key = private_key_obj.exchange(ec.ECDH(), sender_public_key_obj)
     # Now let's do AES-CBC with this, including the hmac matching (modeled after eccrypto code).
     aes_keyhash = hashlib.sha512(aes_shared_key).digest()
@@ -42,3 +42,14 @@ def js_to_py_encrypted(js_encrypted_msg):
     }
 
     return py_encrypted_obj
+
+def to_int(primitive: Union[bytes, bytearray, int, bool] = None) -> int:
+    """
+    From https://github.com/ethereum/eth-utils/blob/master/eth_utils/conversions.py#L55-L83
+    """
+    if isinstance(primitive, (bytes, bytearray)):
+        return int.from_bytes(primitive,'big')
+    elif isinstance(primitive, (int, bool)):
+        return int(primitive)
+    else:
+        raise TypeError('Primitive must be bytes, bytearray, int or bool.')
