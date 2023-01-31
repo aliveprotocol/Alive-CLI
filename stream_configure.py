f76a2fb54a13ce413488e958420ef4e95ac10d02
techcoderx <engtianxi@outlook.com>
2023-01-31 16:23:29 +0800

    configure preferred gateway

--- stream_configure.py
+++ stream_configure.py
@@ -6,8 +6,8 @@ if '.' in __name__:
 else:
     import constants
 
-if len(sys.argv) != 7:
-    print('Usage: python3 ' + sys.argv[0] + ' <network> <api_node> <link> <alivedb_pubkey> <username> <private_key>')
+if len(sys.argv) < 7:
+    print('Usage: python3 ' + sys.argv[0] + ' <network> <api_node> <link> <alivedb_pubkey> <username> <private_key> <preferred_gateway>')
     sys.exit(0)
 
 api = sys.argv[2]
@@ -15,6 +15,10 @@ link = sys.argv[3]
 pub = sys.argv[4]
 sender = sys.argv[5]
 key = sys.argv[6]
+gw = None
+
+if len(sys.argv) >= 8:
+    gw = sys.argv[7]
 
 if sys.argv[1] == 'avalon':
     # Avalon
@@ -55,6 +59,8 @@ elif sys.argv[1] == 'hive':
         'pub': pub,
         'l2': 'gundb'
     }
+    if gw:
+        json_data['gw'] = gw
 
     pprint(hive_client.custom_json(constants.hive_custom_json_id,json_data,required_posting_auths=[sender]))
 else:
