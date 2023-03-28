95a529fb28e4319fde6610c3e8eb11b05695438a
techcoderx <engtianxi@outlook.com>
2023-03-28 19:19:52 +0800

    Update aliverc.py

--- aliverc.py
+++ aliverc.py
@@ -29,27 +29,9 @@ op_size = rc.get_tx_size(op)
 result = rc.custom_json(op_size)
 tx_per_hour = 60/chunk_interval_minutes
 print()
+print('CUSTOM_JSON')
 print('Graphene tx size: ' + str(op_size))
 print('RC per tx: ' + str(result) + ' ' + str(round(result/1000000000,3)) + 'KV')
 print('RC 1hr: ' + str(round(result*tx_per_hour/1000000000,3)) + 'KV')
 print('Hive KVests 5d: ' + str(round(result*tx_per_hour*5*24/1000000000,3)))
-print()
-
-avalon_tx = {
-    'type': 23,
-    'data': {
-        'link': test_link,
-        'src': test_src
-    },
-    'sender': test_username,
-    'ts': 1610713944639,
-    'hash': '1698fe7e753de1810459fa43aee45f9a3ebb4c1d7faa1205596e5f2850089411',
-    'signature': '2hRZLEoKDXrvLywvbTLSbJSU9NLMipWioPEAWz6bxWLPQ1ehnyuSsWeLkNJHJtXgp29NYqBqHDmE6PPWHgkZtHZb'
-}
-
-avalon_tx_size = len(json.dumps(avalon_tx,separators=(',', ':')))
-
-print('Avalon tx size: ' + str(avalon_tx_size))
-print('Bandwidth 1hr: ' + str(avalon_tx_size*12))
-print('Minutes 64KB: ' + str(round(64000/avalon_tx_size*5,2)))
 print()
