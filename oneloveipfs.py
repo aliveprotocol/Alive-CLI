e57905c8748ec24a36fc66bba60e5abcf01b84c5
techcoderx <engtianxi@outlook.com>
2023-03-30 22:49:12 +0800

    remove useless oneloveipfs.py method

--- oneloveipfs.py
+++ oneloveipfs.py
@@ -2,8 +2,6 @@
 OneLoveIPFS hosting service Python API
 """
 import requests
-import base58
-import hashlib
 from beemgraphenebase import ecdsasig
 
 DEFAULT_ENDPOINT = 'https://uploader.oneloveipfs.com'
@@ -22,21 +20,11 @@ def generate_message_to_sign(username: str, network: str, auth_id: str, api: str
         except:
             raise RuntimeError('Could not fetch dynamic global properties')
     elif network == 'avalon' or network == 'avalon':
-        try:
-            count = requests.get(api+'/count').json()['count']
-            bhash = requests.get(api+'/block/'+str(count)).json()['hash']
-            message = message+str(count)+':'+str(bhash)
-        except:
-            raise RuntimeError('Could not fetch latest blockchain info from Avalon')
+        raise RuntimeError('Avalon network is deprecated')
     return message
 
 def sign_message(message: str, priv_key: str) -> str:
     return message+':'+ecdsasig.sign_message(message=message,wif=priv_key).hex()
-
-def sign_message_avalon(message: str, priv_key: str) -> str:
-    k = bytearray([0x80]) + bytearray(base58.b58decode(priv_key))
-    wif = base58.b58encode(k + bytearray(hashlib.sha256(hashlib.sha256(bytes(k)).digest()).digest()[:4])).decode('utf-8')
-    return sign_message(message,wif)
 
 def login(signed_msg: str, endpoint: str = DEFAULT_ENDPOINT) -> dict:
     headers = { 'Content-Type': 'text/plain' }
