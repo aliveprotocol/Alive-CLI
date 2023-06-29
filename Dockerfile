0023e99ce734c85e372a9d1b628ae04c3514be19
techcoderx <engtianxi@outlook.com>
2023-06-29 15:56:47 +0800

    dockerize

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Start with a base Python 3.10 image
+FROM python:3.10-slim-buster
+
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libssl-dev \
+    libsm6 \
+    libxext6 \
+    ffmpeg \
+    && rm -rf /var/lib/apt/lists/*
+
+# Working directory in the container
+WORKDIR /app
+
+# Copy project files
+COPY . .
+
+# Install
+RUN pip install . --use-pep517
+
+# Default variables
+ENV ALIVE_BATCH_INTERVAL=60
+
+# Run daemon
+CMD ["alive_docker"]
---
+++ alivecli/scripts/docker_main.py
@@ -0,0 +1,24 @@
+from alivecli.stream_hls import AliveDaemon, AliveInstance
+from alivecli.alivedb import AliveDB
+from signal import signal, SIGINT
+from os import getenv
+
+def main():
+    alivedb_instance = AliveDB(alivedir=getenv('ALIVEDB_ENDPOINT'))
+
+    alive_instance = AliveInstance(
+        data_dir=getenv('ALIVE_DATA_DIR'),
+        protocol='IPFS',
+        upload_endpoint=getenv('ALIVE_UPLOAD_ENDPOINT'),
+        network='hive',
+        api=getenv('ALIVE_BLOCKCHAIN_API'),
+        halive_api=getenv('ALIVE_HALIVE_API'),
+        username=getenv('ALIVE_STREAMER_USERNAME'),
+        private_key=getenv('ALIVE_STREAMER_KEY'),
+        link=getenv('ALIVE_STREAM_LINK'),
+        batch_interval=int(getenv('ALIVE_BATCH_INTERVAL'))
+    )
+
+    alive_daemon = AliveDaemon(instance=alive_instance,alivedb_instance=alivedb_instance)
+    signal(SIGINT,alive_daemon.sigint_handler)
+    alive_daemon.start_worker()
