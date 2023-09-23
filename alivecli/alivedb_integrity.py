8cc5536e93a2657bb27f176707d30e9cf2ea536d
techcoderx <engtianxi@outlook.com>
2023-09-23 11:25:29 +0800

    new alivedb version

--- alivecli/alivedb_integrity.py
+++ alivecli/alivedb_integrity.py
@@ -1,4 +1,5 @@
 # Version -> SHA256 of package.json, package-lock.json and everything in src folder
 integrity = {
-    '0.1.0': '502bcfe2fe5a809368c9dadd4f0876874fbce1e780a0a002c79e116e2eaa2281'
+    '0.1.0': '502bcfe2fe5a809368c9dadd4f0876874fbce1e780a0a002c79e116e2eaa2281',
+    '0.1.1': '5759da7f9df8716bba7f902d282a9d9dd76ad23f9c11054bd62365a6d5ecfa7d'
 }
